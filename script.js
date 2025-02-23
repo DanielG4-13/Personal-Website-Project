2ffa62ec8506db6dbf0396dcad293678603f58ae
Daniel Garibay <danielgaribay366@gmail.com>
2025-02-22 19:51:03 -0600

    #

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-console.log('Hello World!');
-console.log('This is Daniel Garibay');
-console.log('In the Flesh');
-console.log('-ish');
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
 
-
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
